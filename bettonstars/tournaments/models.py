28cd77e064e4e2f58fbec15a2a3d0c596fa79c3d
Никита Молчанов <nylesszpg@gmail.com>
2023-08-28 15:53:51 +0300

    модель турнира

--- bettonstars/tournaments/models.py
+++ bettonstars/tournaments/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Tournaments(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название турнира',
+        help_text='Введите название для вашего турнира'
+    )
+    ticket_price = models.IntegerField(blank=False)
+    password_for_tournament = models.CharField(
+        blank=False,
+        verbose_name='Пароль для входа',
+        help_text='Придумайте пароль для входа участников в турнир')
+    members = models.IntegerField(blank=False)
+    start_date_time = models.DateField()
+    end_date_time = models.DateField()
+    prize_fund = models.IntegerField(blank=False)
+    text = models.TextField(
+        verbose_name='Текст турнира',
+        help_text='Расскажите, какие матчи будет для прогнозов'
+    )
+    image = models.ImageField(
+        'Картинка',
+        upload_to='tournaments/',
+        blank=True
+    )
+
+    class Meta:
+        ordering = ['-start_date_time']
+    
+    def __str__(self):
+        # выводим текст поста
+        return self.name
